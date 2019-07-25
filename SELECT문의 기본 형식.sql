df67eaef14b44558e21744812ee9dfb910fd29cd
Chocobe <nice_10bird@naver.com>
2019-07-25 23:04:21 +0900

    <master> : Fixed "SELECT문의 기본 형식.sql"

--- SELECT문의 기본 형식.sql
+++ SELECT문의 기본 형식.sql
@@ -300,3 +300,143 @@ SELECT *
 SELECT *
     FROM EMP
     WHERE COMM IS NULL;
+    
+    
+-- EMP 테이블에서 직속상사가 있는 사원정보를 출력하기
+SELECT *
+    FROM EMP
+    WHERE MGR IS NOT NULL;
+    
+    
+--
+-- UNION 집합연산자
+--
+
+-- EMP 테이블에서 부서번호가 10 또는 20인 사원의 직원번호, 이름, 급여, 부서번호를 출력하기
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 10
+UNION
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 20;
+    
+    
+-- 동일한 결과를 UNION연산자 사용하기
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 10
+UNION
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 10;
+    
+-- 동일한 결과를 UNION ALL연산자 사용하기
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 10
+UNION ALL
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 10;
+    
+    
+--
+-- MINUS연산자
+--
+
+-- EMP 테이블에서 부서번호가 10이 아닌 사원의
+-- 직원번호, 이름, 급여, 부서번호를 출력하기
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+MINUS
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 10;
+    
+    
+--
+-- INTERSECT연산자
+--
+
+-- EMP 테이블에서 부서번호가 10인 사원의
+-- 사원번호, 이름, 급여, 부서번호를 출력하기
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+INTERSECT
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 10;
+    
+    
+-- 연습문제
+-- [1] EMP 테이블을 사용하여 다음과 같이 사원 이름(ENAME)이 S로 끝나는
+-- 사원 데이터를 모두 출력하시오
+SELECT *
+    FROM EMP
+    WHERE ENAME LIKE '%S';
+    
+    
+-- [2] EMP 테이블에서 30번 부서에서 근무하고 있는 사원 중, 직책이
+-- SALESMAN인 사원의 사원번호, 이름, 직책, 급여, 부서번호를 출력하시오
+SELECT EMPNO, ENAME, JOB, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 30 AND JOB = 'SALESMAN';
+    
+SELECT EMPNO, ENAME, JOB, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO = 30
+INTERSECT
+SELECT EMPNO, ENAME, JOB, SAL, DEPTNO
+    FROM EMP
+    WHERE JOB = 'SALESMAN';
+    
+    
+-- [3] EMP 테이블에서 20번, 30번 부서에 근무하고 있는 사원 중,
+-- 급여가 2000초과인 사원을 다음 두가지 방식의 SELECT문을 사용하여
+-- 사원번호, 이름, 급여, 부서번호를 출력하시오
+
+-- [3 - 1] 집합 연산자를 사용하지 않은 방식
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO IN(20, 30) AND SAL > 2000;
+    
+-- [3 - 2] 집합 연산자를 사용한 방식
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE DEPTNO IN(20, 30)
+INTERSECT
+SELECT EMPNO, ENAME, SAL, DEPTNO
+    FROM EMP
+    WHERE SAL > 2000;
+    
+-- [4] NOT BETWEEN A AND B연산자를 쓰지 않고,
+-- 급여 열 값이 2000 이상, 3000이하 범위 이회의 값을
+-- 가진 데이터만 출력하시오
+SELECT *
+    FROM EMP
+    WHERE NOT (SAL >= 2000 AND SAL <= 3000);
+    
+-- [5] 사원 이름에 E가 포함되어 있는 30번 부서의 사원 중
+-- 급여가 1000 ~ 2000 사이가 아닌 사원의
+-- 이름, 사원번호, 급여, 부서번호를 출력하시오
+SELECT ENAME, EMPNO, SAL, DEPTNO
+    FROM EMP
+    WHERE NOT (SAL BETWEEN 1000 AND 2000) AND
+        ENAME LIKE '%E%' AND
+        DEPTNO = 30;
+        
+-- [6] 추가수당이 존재하지 않고 상급자가 있고
+-- 직책이 MANAGER, CLERK인 사원 중에서
+-- 사원 이름의 두번째 글자가 L이 아닌 사원의 정보를 출력하시오
+SELECT *
+    FROM EMP
+    WHERE COMM IS NULL
+INTERSECT
+SELECT *
+    FROM EMP
+    WHERE JOB IN('MANAGER', 'CLERK')
+INTERSECT
+SELECT *
+    FROM EMP
+    WHERE NOT (ENAME LIKE '_L%');
