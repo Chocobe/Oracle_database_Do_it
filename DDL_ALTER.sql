15f5e78b709d1f0c9439df30ddc97bf6537ec07b
Chocobe <nice_10bird@naver.com>
2019-08-01 21:05:48 +0900

    <master> : Fixed "DDL_ALTER.sql" file

--- DDL_ALTER.sql
+++ DDL_ALTER.sql
@@ -7,3 +7,25 @@
 -- 열의 데이터 길이 변경
 -- 등의 테이블 구조를 변경할 수 있다.
 
+-- 연습용 테이블 생성
+CREATE TABLE EMP_ALTER
+    AS SELECT *
+         FROM EMP;
+   
+-- 열 추가하기  
+-- 이름이 HP이고 자료형이 VARCHAR2(20)인 열 추가하기
+ALTER TABLE EMP_ALTER
+  ADD HP VARCHAR2(20);
+  
+-- 열 이름 변경하기
+-- HP열을 TEL로 변경하기
+ALTER TABLE EMP_ALTER
+    RENAME COLUMN HP TO TEL;
+    
+-- 열 자료형 또는 길이 변경하기
+ALTER TABLE EMP_ALTER
+    MODIFY EMPNO NUMBER(5);
+    
+-- 테이블의 특정 열 삭제하기
+ALTER TABLE EMP_ALTER
+    DROP COLUMN TEL;
