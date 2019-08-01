8adace2eed9e5c724587dde47c2f155e66006a64
Chocobe <nice_10bird@naver.com>
2019-08-01 13:09:44 +0900

    <master> : Fixed "트랜잭션_세션_연습문제.sql" file

--- 트랜잭션_세션_연습문제.sql
+++ 트랜잭션_세션_연습문제.sql
@@ -1,2 +1,19 @@
 --
 -- 트랜잭션과 세션 연습문제
+
+-- 연습문제용 테이블 작성
+CREATE TABLE DEPT_HW
+    AS SELECT *
+         FROM DEPT;
+         
+-- DEPTNO = 30 인 행이 LOCK됨
+UPDATE DEPT_HW
+   SET DNAME = 'DATABASE',
+       LOC = 'SEOUL'
+ WHERE DEPTNO = 30;
+ 
+-- 트랜잭션 복구
+ROLLBACK;
+
+SELECT *
+  FROM DEPT_HW;
